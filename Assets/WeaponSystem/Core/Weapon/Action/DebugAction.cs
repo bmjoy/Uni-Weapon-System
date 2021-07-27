524c89d0d11382c69dfb095650bfc762d67c2cdf
NebusokuDev <64105820+summitridgeedu@users.noreply.github.com>
2021-07-27 16:19:44 +0900

    Update .gitignore

---
+++ Assets/WeaponSystem/Core/Weapon/Action/DebugAction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using WeaponSystem.Core.Debug;
+using WeaponSystem.Core.Movement;
+using WeaponSystem.Core.Weapon.Magazine;
+
+namespace WeaponSystem.Core.Weapon.Action
+{
+    public class DebugAction : IWeaponAction
+    {
+        public void Injection(Transform parent, IMagazine magazine)
+        {
+            parent.name.Log();
+        }
+
+        public void Action(bool isAction, IPlayerContext context) => $"isAction: {isAction.ToString()}".Log();
+
+        public void AltAction(bool isAltAction, IPlayerContext context) => $"isAltAction: {isAltAction.ToString()}".Log();
+    }
+}
